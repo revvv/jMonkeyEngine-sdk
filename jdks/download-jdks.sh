4aa3c48123ac9d2dea483094b0793d2d68370cda
MeFisto94 <MeFisto94@users.noreply.github.com>
2017-11-12 14:52:45 +0100

    Debug

--- jdks/download-jdks.sh
+++ jdks/download-jdks.sh
@@ -95,6 +95,7 @@ function unpack_mac_jdk {
         7z x 4.hfs > /dev/null
         #install_xar
         #./xar-1.5.2/src/xar -xf JDK*/JDK*.pkg
+        ls -lsh
         7z x JDK*.pkg 
         ls -lsh
         cd jdk1*.pkg
