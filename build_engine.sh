8cbbdadc2c27f94c9c73ad2904d7d847cb261bb5
MeFisto94 <MeFisto94@users.noreply.github.com>
2018-01-11 02:16:20 +0100

    Fix build_engine.sh: The Engine GIT Tags are "v3.2.0-stable", whereas the POM Version is 3.2.0-stable

--- build_engine.sh
+++ build_engine.sh
@@ -5,7 +5,7 @@
 echo "Downloading the Engine, this may take some time"
 if [ "x$TRAVIS" != "x" ] && [ "x$TRAVIS_TAG" != "x" ]; then
     # Extract the engine version from the sdk branch tag.
-    git clone -b $(echo "$TRAVIS_TAG" | sed -n 's/v\(.\+\)-sdk.\+/\1/p') --single-branch --depth 10 https://github.com/jMonkeyEngine/jMonkeyEngine/ engine # single-branch requires git > 1.7.10, if you see an error, just leave it out.
+    git clone -b $(echo "$TRAVIS_TAG" | sed -n 's/\(v.\+\)-sdk.\+/\1/p') --single-branch --depth 10 https://github.com/jMonkeyEngine/jMonkeyEngine/ engine # single-branch requires git > 1.7.10, if you see an error, just leave it out.
 else
     git clone -b master --single-branch --depth 10 https://github.com/jMonkeyEngine/jMonkeyEngine/ engine # single-branch requires git > 1.7.10, if you see an error, just leave it out.
 fi
