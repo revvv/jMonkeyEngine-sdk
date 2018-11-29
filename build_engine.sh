f424d292b443bb9502c89396d7698563a1ecb972
grizeldi <grizeldi@gmail.com>
2018-11-29 17:30:52 +0100

    Version bump and build_engine.sh fixes for existing installs

--- build_engine.sh
+++ build_engine.sh
@@ -2,14 +2,20 @@
 # This Shell Script will build and use the latest jMonkeyEngine git version, so there might be some undiscovered engine bugs, watch out!
 # Also if you want to revert to releases and bintray builds, you need to uninstall them from your local maven repo...
 
-echo "Downloading the Engine, this may take some time"
-if [ "x$TRAVIS" != "x" ] && [ "x$TRAVIS_TAG" != "x" ]; then
-    # Extract the engine version from the sdk branch tag.
-    git clone -b $(echo "$TRAVIS_TAG" | sed -n 's/\(v.\+\)-sdk.\+/\1/p') --single-branch --depth 10 https://github.com/jMonkeyEngine/jMonkeyEngine/ engine # single-branch requires git > 1.7.10, if you see an error, just leave it out.
+if [ ! -d "engine" ]; then
+    echo "Downloading the Engine, this may take some time"
+    if [ "x$TRAVIS" != "x" ] && [ "x$TRAVIS_TAG" != "x" ]; then
+        # Extract the engine version from the sdk branch tag.
+        git clone -b $(echo "$TRAVIS_TAG" | sed -n 's/\(v.\+\)-sdk.\+/\1/p') --single-branch --depth 10 https://github.com/jMonkeyEngine/jMonkeyEngine/ engine # single-branch requires git > 1.7.10, if you see an error, just leave it out.
+    else
+        git clone -b master --single-branch --depth 10 https://github.com/jMonkeyEngine/jMonkeyEngine/ engine # single-branch requires git > 1.7.10, if you see an error, just leave it out.
+    fi
+    cd engine
 else
-    git clone -b master --single-branch --depth 10 https://github.com/jMonkeyEngine/jMonkeyEngine/ engine # single-branch requires git > 1.7.10, if you see an error, just leave it out.
+    echo "Engine already cloned, pulling updates."
+    cd engine
+    git pull
 fi
-cd engine
 # git checkout tags/v3.1.0-beta2 # To use this, leave out depth and change -b to a branch.
 
 #echo "Patching the Engine...."
